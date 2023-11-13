958b8847b7df34b44b8d5d0bdc5d0fc4426525e4
Damian Mroczek <damianmroczek@yahoo.com>
2023-11-13 18:14:25 +0700

    add searchBar header image

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ const menu = document.querySelector('#menubar');
 const mynav = document.querySelector(".navbar");
 const myUser = document.querySelector('#user');
 const myForm = document.querySelector(".search-form");
+const mySearch = document.querySelector('#search');
+const searchBar = document.querySelector(".searchbar");
 
 menu.onclick = () => {
 	menu.classList.toggle('fa-times');
@@ -9,4 +11,7 @@ menu.onclick = () => {
 }
 myUser.onclick = () => {
 	myForm.classList.toggle('active');
+}
+mySearch.onclick = () => {
+	searchBar.classList.toggle('active');
 }
